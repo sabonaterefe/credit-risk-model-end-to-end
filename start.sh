980903e439da73b6f1c9693c477345df0c6765db
sabonaterefe <sabonaterefe@gmail.com>
2025-07-02 14:01:30 +0300

    Save local changes before pulling from Hugging Face

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Start FastAPI backend on port 8000
+# Starting FastAPI backend on port 8000
 uvicorn src.api.main:app --host 0.0.0.0 --port 8000 &
 
-# Start Streamlit frontend on port 7860 (required by Hugging Face)
+# Starting  Streamlit frontend on port 7860 (required by Hugging Face)
 streamlit run streamlit_app.py --server.port 7860 --server.address 0.0.0.0
+
